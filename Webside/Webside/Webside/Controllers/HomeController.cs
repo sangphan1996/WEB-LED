a1ba937d4dc200ac932b53c225a16be177e4cd5c
sangphan1996 <41358904+sangphan1996@users.noreply.github.com>
2018-09-20 17:01:37 +0700

    tao moi

---
+++ Webside/Webside/Webside/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webside.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult cauhinh()
+        {
+            return View();
+        }
+        public ActionResult quanlytaikhoan()
+        {
+            return View();
+        }
+        public ActionResult nhaplieu()
+        {
+            return View();
+        }
+        public ActionResult trogiup()
+        {
+            return View();
+        }
+       public ActionResult dangki()
+        {
+            return View();
+        }
+        public ActionResult dangnhap()
+        {
+            return View();
+        }
+        public ActionResult bannang()
+        {
+            return View();
+        }
+        public ActionResult dangxuat()
+        {
+            return View();
+        }
+	}
+}
---
+++ Webside/Webside/Webside/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webside.Startup))]
+namespace Webside
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
